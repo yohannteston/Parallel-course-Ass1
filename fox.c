9e088262291fe4dd52b1ddd36b2ae67e43c879bd
yohannteston <yohann.teston@free.fr>
2010-04-22 21:06:35 +0200

    Improve the code using non blocking communication (yes, do that, fucking computer :P)

--- fox.c
+++ fox.c
@@ -53,17 +53,17 @@ main (char **argv, int argc)
 {
   int p, sp,k ;
   int coords[2], pos[2], dims[2]={0,0}, periods[2]={0,0};
-  int myid,i,j,subrankrow,subrankcol,rank,n;
-  double *myB, *myA, *A, *B, *C, *tmp, *AA ;
+  int i,j,subrankrow,subrankcol,n,rank;
+  double *myB, *myA, *A, *B, *C, *tmp, *A_tmp, *MA, *MB ;
+  double timer;
   MPI_Status status;
   MPI_Datatype block;
   MPI_Comm proc_grid, proc_row, proc_col;
-  MPI_Request send, recv;
+  MPI_Request request_1, request_2;
 
   MPI_Init(&argc, &argv);               /* Initialize MPI               */
-  double timer1 =MPI_Wtime();
+  timer =MPI_Wtime();
   MPI_Comm_size(MPI_COMM_WORLD, &p); /* Get the number of processors */
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank); /* Get my number                */
 
   sp = sqrt (p) ;
   assert (p == sp*sp) ;
@@ -82,9 +82,9 @@ main (char **argv, int argc)
   /* Create a virtual 2D-grid topology */
   MPI_Dims_create(p, 2, dims);
   MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 1, &proc_grid);
-  MPI_Comm_rank(proc_grid, &myid);    /* Note: use proc_grid */
+  MPI_Comm_rank(proc_grid, &rank);    /* Note: use proc_grid */
   /* gets the coords of the processor in the grid */
-  MPI_Cart_coords(proc_grid, myid, 2, coords); 
+  MPI_Cart_coords(proc_grid, rank, 2, coords); 
 
   /* Create a communicator for each row */
   MPI_Comm_split(proc_grid, coords[0], coords[1], &proc_row);
@@ -92,14 +92,14 @@ main (char **argv, int argc)
 
   /* Create a communicator for each column */ 
   MPI_Comm_split(proc_grid, coords[1], coords[0], &proc_col);
-  MPI_Comm_rank(proc_col, &subrankcol); 
+  MPI_Comm_rank(proc_col, &subrankcol);
 
-  /* Create a datatype to represent a block  */
-  MPI_Type_vector(n, n, size, MPI_DOUBLE, &block);
-  MPI_Type_commit(&block);
+  if (rank == 0){
 
-  if (!coords[0] && !coords[1]){
-      double *MA, * MB;
+      /* Create a datatype to represent a block  */
+    MPI_Type_vector(n, n, size, MPI_DOUBLE, &block);
+    MPI_Type_commit(&block);
+
       srand(time(NULL));
       MA = malloc(size*size*sizeof(double));
       MB = malloc(size*size*sizeof(double));
@@ -111,43 +111,20 @@ main (char **argv, int argc)
       init(MB, size);
       printf("Matrix B:\n");
       print(MB,size);
-      // the matrices are created, we send each block to its "owner"
+      // the matrices are created, we send each block to its "owner" using non blocking communication
       for (i = 0; i < sp; i++ )
 	for (j = 0; j < sp; j++ ) {
-	  if ( !i && !j )
-	    {
-	      int i,j ;
-	      for(i=0; i < n; i++ )
-		for(j=0; j < n; j++ )
-		  {
-		    myA[i*n+j] = MA[i*size + j] ;
-		    myB[i*n+j] = MB[i*size + j] ;
-		  }						
-	    }
-	  else
-	    {
-	      int rank ;
+	      int target ;
 	      pos[0] = i ;
 	      pos[1] = j ;
 
-	      MPI_Cart_rank(proc_grid, pos, &rank) ;
-	      MPI_Send(&MA[(i*size*n)+(j*n)], 1, block, rank , 111, proc_grid);
-	      MPI_Send(&MB[(i*size*n)+(j*n)], 1, block, rank , 111, proc_grid);
-	    }
+	      MPI_Cart_rank(proc_grid, pos, &target) ;
+	      MPI_Isend(&MA[(i*size*n)+(j*n)], 1, block, target , 1, proc_grid,&request_1);
+	      MPI_Isend(&MB[(i*size*n)+(j*n)], 1, block, target , 2, proc_grid,&request_2);
 	}
-      free(MA); free(MB); // the big matrices are not needed anymore
     }
-  else
-    {
-
-      int rank ;
-      pos[0] = 0 ;
-      pos[1] = 0 ;
-
-      MPI_Cart_rank(proc_grid, pos, &rank) ;
-      MPI_Recv(myA, n*n, MPI_DOUBLE, rank, 111,  proc_grid, &status );
-      MPI_Recv(myB, n*n, MPI_DOUBLE, rank, 111,  proc_grid, &status );
-    }
+  MPI_Recv(myA, n*n, MPI_DOUBLE, 0, 1,  proc_grid, &status );
+  MPI_Recv(myB, n*n, MPI_DOUBLE, 0, 2,  proc_grid, &status );
 
   /* everyone has its blocks, the algorithm can start */
   for (k = 0; k < sp; k++)
@@ -157,20 +134,19 @@ main (char **argv, int argc)
       if (m == subrankrow)
 	{
 	  MPI_Bcast (myA, n*n, MPI_DOUBLE, m, proc_row) ;
-	  //memcpy (A, myA, n*n*sizeof(double)) ;
-	  AA = myA;
+	  A_tmp = myA;
 	}
       else
 	{
 	  MPI_Bcast (A, n*n, MPI_DOUBLE, m, proc_row) ;
-	  AA = A;
+	  A_tmp = A;
 	}
       //before the computation, let's start the communication
-      MPI_Isend(myB, n*n, MPI_DOUBLE, ((subrankcol+sp-1)%sp), subrankcol+sp*k, proc_col,&send);
-      MPI_Irecv(B, n*n, MPI_DOUBLE, ((subrankcol+1)%sp), ((subrankcol+1)%sp)+sp*k, proc_col, &recv);
-      matmult (AA, myB, C, n) ;
-      MPI_Wait(&send, &status);
-      MPI_Wait(&recv, &status);
+      MPI_Isend(myB, n*n, MPI_DOUBLE, ((subrankcol+sp-1)%sp), subrankcol+sp*k, proc_col,&request_1);
+      MPI_Irecv(B, n*n, MPI_DOUBLE, ((subrankcol+1)%sp), ((subrankcol+1)%sp)+sp*k, proc_col, &request_2);
+      matmult (A_tmp, myB, C, n) ;
+      MPI_Wait(&request_1, &status);
+      MPI_Wait(&request_2, &status);
       // now, we work using the newly received B
       tmp = myB;
       myB = B;
@@ -178,48 +154,25 @@ main (char **argv, int argc)
     }
 
   // gathering the results
-  if (!coords[0] && !coords[1])
+  MPI_Isend(C, n*n, MPI_DOUBLE, 0, 1, proc_grid, &request_1);
+  if (rank == 0)
     {
-      double* MC = calloc (size*size, sizeof(double)) ;
-      for (i = 0; i < sp; i++ )
-	for (j = 0; j < sp; j++ ) {
-	  if ( !i && 	!j )
-	    {
-	      int i,j ;
-	      for(i=0; i < n; i++ )
-		for(j=0; j < n; j++ )
-		  {
-		    MC[(i*size)+ j] = C[i*n + j] ;
-		  }
-	    }
-	  else
-	    {
-	      int rank ;
-	      pos[0] = i ;
-	      pos[1] = j ;
+      double *MC;
+      MC = malloc(size*size*sizeof(double));
 
-	      MPI_Cart_rank(proc_grid, pos, &rank) ;
-	      MPI_Recv(&MC[(i*size*n)+(j*n)], 1, block, rank, 111,  proc_grid, &status );
-	    }
-	}
-      // displays the result matrix
-      if (!coords[0] && !coords[1]) {
-	printf("Result:\n");
-	print(MC,size);
-	printf("time needed: %lf\n",MPI_Wtime() - timer1);
-	free(MC);
+      for(i=0; i < size; i++){
+	MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, proc_grid, &status);
+	MPI_Cart_coords(proc_grid, status.MPI_SOURCE, 2, coords);
+	MPI_Recv(&MC[(coords[0]*size*n)+(coords[1]*n)], 1, block, status.MPI_SOURCE, 1, proc_grid, &status); 
       }
-    }
-  else
-    {
-      int rank ;
-      pos[0] = 0 ;
-      pos[1] = 0 ;
-
-      MPI_Cart_rank(proc_grid, pos, &rank) ;
-      MPI_Send(C, n*n, MPI_DOUBLE, rank, 111,  proc_grid);
+      /* displays the result */
+      printf("Result:\n");
+      print(MC, size);
+      printf("\nTime needed: %lf\n",MPI_Wtime()-timer);
+      free(MA); free(MB); free(MC);
     }
 
+  MPI_Wait(&request_1, &status);
   MPI_Finalize() ;
   free(myA); free(myB); free(A); free(B); free(C);
 	
