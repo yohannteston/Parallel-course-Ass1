839ee4182f074bc5fe899d9c1a2878156ba76df1
Matthieu Maury <mama5321@spjutet.it.uu.se>
2010-04-25 13:02:39 +0200

    Add launching script and nodes file

---
+++ launch.rb
@@ -0,0 +1,10 @@
+#!/usr/local/bin/ruby
+[100,200, 400,600,800,1000,1200,1440].each do |size|
+	[1,4,9,16,25,36,49,64].each do |n|
+		if (n <= 8)
+			10.times { puts "#{size}  #{n}  " + `mpirun -np #{n} ./a.out #{size}` }
+		else
+			10.times { puts "#{size}  #{n}  " + `mpirun -hostfile nodes -mca plm_rsh_agent rsh -np #{n} ./a.out #{size}` }
+		end
+	end
+end
